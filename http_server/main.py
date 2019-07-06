9439cc4d0a9313753855950cd8b88edc3a8bd102
Aneta Włodarczyk <anetka.wlodarczyk@gmail.com>
2019-07-06 23:38:24 +0200

    Create table if not exists when setting up project

--- http_server/main.py
+++ http_server/main.py
@@ -1,9 +1,14 @@
+import asyncio
+
 from databases import Database
 from environs import Env
 from sanic import Sanic
 from sanic.response import json
+from sqlalchemy import create_engine, MetaData, Table
 
+from http_server import worker, settings
 from http_server.settings import Settings
+from http_server.tables import urls
 
 app = Sanic()
 
@@ -22,14 +27,23 @@ def setup_database():
     async def disconnect_from_db(*args, **kwargs):
         await app.db.disconnect()
 
+#TODO: Can be done manually when setting up environment and put in README
+@app.listener('after_server_start')
+def create_table_if_not_exists():
+    engine = create_engine(settings.DB_URI)
+    if not engine.has_table(engine, 'urls'):
+        metadata = MetaData(engine)
+        metadata.create_all(tables=urls)
 
 def init():
     env = Env()
     env.read_env()
 
+    # probably I should use here app.create_server method
     config = app.config.from_object(Settings)
 
     setup_database()
+    create_table_if_not_exists()
 
     app.run(config)
 

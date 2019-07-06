9ac831cd2cf5a645d14217c2653e2b62b5743f84
Aneta Włodarczyk <anetka.wlodarczyk@gmail.com>
2019-07-06 23:47:45 +0200

    Get url and get url history with tests

--- http_server/routes.py
+++ http_server/routes.py
@@ -2,6 +2,7 @@ from sanic import response
 from sanic.exceptions import InvalidUsage, NotFound, PayloadTooLarge
 from sanic.response import json
 from sqlalchemy import create_engine
+from sqlalchemy.orm.exc import NoResultFound
 
 from http_server import settings
 from http_server.main import app
@@ -25,5 +26,35 @@ def post(request):
         url=url, interval=interval, response=response)
     result = insert_query.execute()
     return json({'id': result.inserted_primary_key})
+
+@app.route("/api/fetcher", methods=['GET'])
+def get(request):
+    query = urls.select()
+    rows = request.app.db.fetch_all(query)
+    return [
+        {'id': row['id'],
+         'url': row['url'],
+         'interval': row['interval']}
+        for row in rows
+    ]
+
+@app.route("/api/fetcher/<id>/history", methods=['GET'])
+def get_url(request, id):
+    try:
+        int(id)
+    except ValueError:
+        raise InvalidUsage('Incorrect id format provided', status_code=400)
+
+    query = urls.select(urls).where(urls.c.id == id)
+    try:
+        rows = request.app.db.fetch_all(query)
+    except NoResultFound:
+        raise NotFound('Result not found', status_code=404)
+
+    return [
+        {'response': row['response'],
+         'created_at': row['created_at']}
+        for row in rows
+    ]
 
 
--- http_server/tests.py
+++ http_server/tests.py
@@ -18,3 +18,21 @@ def test_post_413_exception():
     request, response = app.test_client.post("/api/fetcher", data=open("test_file_1mb.txt", 'rb'))
     assert response.status == 413
 
+def test_get():
+    request, response = app.test_client.get('/api/fetcher')
+    assert len(response) == 2
+
+def test_get_url():
+    request, response = app.test_client.get_url('/api/fetcher/1/history')
+    assert len(response) == 1
+    assert 'response' in response[0].keys()
+
+def test_get_400_exception():
+    invalid_id = "invalid_id"
+    request, response = app.test_client.get_url(f"/api/fetcher/{invalid_id}/history")
+    assert response.status == 400
+
+def test_get_404_exception():
+    not_existing_id = 1000000
+    request, response = app.test_client.get_url(f"/api/fetcher/{not_existing_id}/history")
+    assert response.status == 404
